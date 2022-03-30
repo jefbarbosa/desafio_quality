b9caa67df31c7e79987e16f9644ba981efdf40c5
jeferson <jeferson.barbosa@mercadolivre.com>
2022-03-30 10:58:18 -0300

    test: Added one more integration test

--- src/test/java/br/com/meli/desafio_quality/PropertyIntegration.java
+++ src/test/java/br/com/meli/desafio_quality/PropertyIntegration.java
@@ -4,11 +4,11 @@ import br.com.meli.desafio_quality.dto.DistrictDTO;
 import br.com.meli.desafio_quality.dto.PropertyDTO;
 import br.com.meli.desafio_quality.dto.RoomDTO;
 import br.com.meli.desafio_quality.entity.Property;
-import br.com.meli.desafio_quality.entity.Room;
 import br.com.meli.desafio_quality.repository.PropertyRepository;
 import br.com.meli.desafio_quality.service.PropertyService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,8 +21,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,8 +39,38 @@ public class PropertyIntegration {
     @Autowired
     private PropertyRepository propertyRepository;
 
+    @Autowired
+    private PropertyService propertyService;
+
+    @BeforeEach
+    private void initConfiguration() throws Exception {
+        DistrictDTO districtDTO1 = new DistrictDTO("Brooklyn", BigDecimal.valueOf(18000));
+        DistrictDTO districtDTO2 = new DistrictDTO("Moema", BigDecimal.valueOf(14000));
+
+        List<RoomDTO> roomsDTO1 = Arrays.asList(new RoomDTO("kitchen", 10.0, 5.0),
+                new RoomDTO("living room", 20.0, 5.0));
+
+        List<RoomDTO> roomsDTO2 = Arrays.asList(new RoomDTO("kitchen", 10.0, 4.0),
+                new RoomDTO("living room", 15.0, 5.0),
+                new RoomDTO("bedroom", 5.0, 5.0));
+
+        PropertyDTO propertyDTO1 = new PropertyDTO(null, "Brooklyn Village", districtDTO1, roomsDTO1);
+        PropertyDTO propertyDTO2 = new PropertyDTO(null, "Moema Palace", districtDTO2, roomsDTO2);
+
+        mockMvc.perform(post("/property/insert")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(propertyDTO1)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/property/insert")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(propertyDTO2)))
+                .andExpect(status().isCreated());
+    }
+
+
     @Test
-    public void insertPropertyAndReturnDtoTest() throws Exception {
+    public void insertPropertyAndCheckDto() throws Exception {
         DistrictDTO districtDTO = new DistrictDTO("Tijuca", BigDecimal.valueOf(15000));
         List<RoomDTO> roomsDTO = Arrays.asList(new RoomDTO("kitchen", 10.0, 5.0),
                 new RoomDTO("living room", 20.0, 5.0));
@@ -58,7 +90,47 @@ public class PropertyIntegration {
 
         Property property = propertyRepository.getProperty(propertyDtoResponse.getId());
         assertEquals("Tijuca Village", property.getName());
+
+        Property propertyNull = propertyRepository.getProperty(propertyDtoResponse.getId()+"-XYZ12345-ABCD56789");
+        assertNull(propertyNull.getName());
     }
+
+    @Test
+    public void insertInvalidRoomDimensionsProperty() throws Exception {
+        //TODO - Validate Invalid Insertions
+
+        DistrictDTO districtDTO = new DistrictDTO("Tijuca", BigDecimal.valueOf(15000));
+        List<RoomDTO> roomsDTO = Arrays.asList(new RoomDTO("kitchen", 10.0, 5.0),
+                new RoomDTO("living room", -2.0, -2.0));
+
+        PropertyDTO propertyDTO = new PropertyDTO(null, "Tijuca Village", districtDTO, roomsDTO);
+
+        MvcResult postResult = mockMvc.perform(post("/property/insert")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(propertyDTO)))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+    }
+
+    @Test
+    public void getAllPropertiesTest() throws Exception{
+        List<PropertyDTO> propertyList = propertyService.getAllProperties();
+        assertEquals(2, propertyList.size());
+
+        MvcResult postResult = mockMvc.perform(get("/property/get-all-properties"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String response = postResult.getResponse().getContentAsString();
+        List<PropertyDTO> propertyDtoResponseList = objectMapper.readValue(response, new TypeReference<List<PropertyDTO>>() {});
+
+        PropertyDTO propertyDTO = propertyDtoResponseList.get(0);
+        assertEquals("Brooklyn", propertyDTO.getDistrict().getName());
+
+    }
+
+
 
 
 }
