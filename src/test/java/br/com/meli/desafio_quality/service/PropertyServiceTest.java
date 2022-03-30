a79f0909f8603760073e158a5d73eb5f6dc8a468
Lucas Troleiz Lopes <lucas.troleiz@mercadolivre.com>
2022-03-30 15:33:21 -0300

    test: test calculate total area and calculate price property

--- src/test/java/br/com/meli/desafio_quality/service/PropertyServiceTest.java
+++ src/test/java/br/com/meli/desafio_quality/service/PropertyServiceTest.java
@@ -1,6 +1,9 @@
 package br.com.meli.desafio_quality.service;
 
 import br.com.meli.desafio_quality.dto.LargestRoomAreaDTO;
+import br.com.meli.desafio_quality.dto.PropertyDTO;
+import br.com.meli.desafio_quality.dto.PropertyPriceDTO;
+import br.com.meli.desafio_quality.dto.PropertyTotalAreaDTO;
 import br.com.meli.desafio_quality.entity.District;
 import br.com.meli.desafio_quality.entity.Property;
 import br.com.meli.desafio_quality.entity.Room;
@@ -48,4 +51,40 @@ public class PropertyServiceTest {
         assertEquals("kitchen", largestRoomAreaDTO.getRoomName());
 
     }
+    @Test
+    public void calculateTotalAreaPropertyTest() {
+            List<Room> rooms = Arrays.asList(
+                    new Room("kitchen", 300.0, 10.0),
+                    new Room("bedroom", 20.0, 10.0),
+                    new Room("living room", 10.0, 10.0)
+            );
+            District district = new District("Tijuca", BigDecimal.valueOf(100.0));
+            Property property = new Property("XYZ-12345", "Tijuca",district, rooms);
+
+            Mockito.when(propertyRepository.getProperty("XYZ-12345")).thenReturn(property);
+
+            PropertyTotalAreaDTO propertyTotalAreaDTO = propertyService.calculateTotalArea(property.getId());
+
+            assertEquals(3300.0, propertyTotalAreaDTO.getTotalArea());
+
+    }
+    @Test
+    public void calculateProperty() {
+        List<Room> rooms = Arrays.asList(
+                new Room("kitchen", 300.0, 10.0),
+                new Room("bedroom", 20.0, 10.0),
+                new Room("living room", 10.0, 10.0)
+        );
+        District district = new District("Tijuca", BigDecimal.valueOf(100.0));
+        Property property = new Property("XYZ-12345", "Tijuca",district, rooms);
+
+        Mockito.when(propertyRepository.getProperty("XYZ-12345")).thenReturn(property);
+
+        PropertyPriceDTO propertyPriceDTO = propertyService.calculateProperty(property.getId());
+
+        assertEquals(new BigDecimal("330000.00"), propertyPriceDTO.getPrice());
+
+    }
+
+
 }
