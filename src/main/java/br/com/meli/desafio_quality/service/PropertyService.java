b2a908eef8a4965149ce20115ac64bc131ae0d16
Jederson Carvalho Macedo <jederson.macedo@mercadolivre.com>
2022-03-31 17:13:10 -0300

    docs: add javadocs in PropertyService

--- src/main/java/br/com/meli/desafio_quality/service/PropertyService.java
+++ src/main/java/br/com/meli/desafio_quality/service/PropertyService.java
@@ -17,35 +17,72 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Classe Service responsável pelos serviços do recurso Property
+ * @author Jeferson Barbosa
+ * @author Arthur Guedes
+ * @author Lucas Troleiz
+ * @author Igor Nogueira
+ */
 @Service
 @AllArgsConstructor
 public class PropertyService {
 
+    /**
+     * {@link PropertyRepository Repository} de property injetado
+     */
     @Autowired
     private PropertyRepository propertyRepository;
 
+    /**
+     * {@link DistrictService Service} de property injetado
+     */
     @Autowired
     private DistrictService districtService;
 
+    /**
+     * Insere uma nova propriedade
+     * @param propertyDTO Uma propriedade
+     * @return Propriedade inserida
+     */
     public PropertyDTO insertProperty(PropertyDTO propertyDTO) {
         districtService.findDistrict(propertyDTO.getDistrict().getName());
 
         return PropertyDTO.propertyToDTO(propertyRepository.addProperty(PropertyDTO.dtoToProperty(propertyDTO)));
     }
 
+    /**
+     * Obtém a lista de propriedades
+     * @return Lista de propriedades
+     */
     public List<PropertyDTO> getAllProperties() {
         return propertyRepository.getAllProperties().stream().map(PropertyDTO::propertyToDTO).collect(Collectors.toList());
     }
 
+    /**
+     * Calcula a área de um cômodo
+     * @param room Cômodo informado para o cálculo da área
+     * @return Área do cômodo informado
+     */
     private Double calculateRoomArea(Room room) {
         return room.getRoomLength() * room.getRoomWidth();
     }
 
+    /**
+     * Calcula o preço de uma popriedade
+     * @param property Propriedade informada para o cálculo do preço
+     * @return Preço da propriedade informada
+     */
     private BigDecimal calculatePropertyPrice(Property property) {
 
          return property.getDistrict().getValueDistrictM2().multiply(BigDecimal.valueOf(calculateTotalArea(property.getId()).getTotalArea()));
     }
 
+    /**
+     * Calcula a área total de uma propriedade
+     * @param id Id da propriedade que terá área total calculada
+     * @return Área total da propriedade informada
+     */
     public PropertyTotalAreaDTO calculateTotalArea(String id) {
         Property property = propertyRepository.getProperty(id);
 
@@ -55,6 +92,11 @@ public class PropertyService {
         return new PropertyTotalAreaDTO(id, property.getName(), totalPropertyArea);
     }
 
+    /**
+     * Busca pelo maior cômodo de uma propriedade
+     * @param propertyId Id da propriedade que terá o maior cômodo buscado
+     * @return Maior cômodo da propriedade informada
+     */
     public LargestRoomAreaDTO findLargestRoom(String propertyId) {
         Property property = propertyRepository.getProperty(propertyId);
 
@@ -63,6 +105,11 @@ public class PropertyService {
         return new LargestRoomAreaDTO(property.getName(), room.getRoomName(), calculateRoomArea(room));
     }
 
+    /**
+     * Calcula a área de cada cômodo de uma propriedade
+     * @param propertyId Id da propriedade que terá a área dos cômodos calculada
+     * @return Áreas dos cômodos da propriedade informada
+     */
     public RoomAreasDTO calculateAreaRooms(String propertyId) {
         Property property = propertyRepository.getProperty(propertyId);
 
@@ -73,6 +120,11 @@ public class PropertyService {
 
     }
 
+    /**
+     * Calcula o preço de uma popriedade
+     * @param propertyId Id da propriedade que terá o preço calculado
+     * @return Preço da propriedade informada
+     */
     public PropertyPriceDTO calculateProperty(String propertyId) {
         Property property = propertyRepository.getProperty(propertyId);
         return new PropertyPriceDTO(property.getName(), calculatePropertyPrice(property));
